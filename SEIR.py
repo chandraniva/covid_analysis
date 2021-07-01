6f5a65eefa0143c29ac6ee8d498458cdf47a1e18
SanchayAgarwal <85455971+SanchayAgarwal@users.noreply.github.com>
2021-07-01 23:55:42 +0530

    Quarantine based model code upload

--- SEIR.py
+++ SEIR.py
@@ -1,131 +1,90 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 14 18:35:05 2021
-@author: Chandraniva
-"""
-
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
-import csv
 
-def model(y,t,beta,N,sigma,gamma,mu):
+def fear_function(I,dIdt,t):
+    beta2 = 0.1 
+    fear = I**2*beta2 - t*100
+    return fear
+
+
+def dydt(t,y):
     [S, I, R, Q] = y
-    
     dIdt = beta*I*S/N - gamma*I
     dRdt = gamma*I          
-    dSdt = - beta*I*S/N - S*dIdt
-    dQdt =  S*dIdt
+    dSdt = - beta*I*S/N 
+    dQdt = 0    
     dydt = [dSdt, dIdt, dRdt, dQdt]
     
-    return dydt
+    return np.array(dydt)
 
-def rk4(x0, y0, x, h,beta,N,sigma,gamma,mu):
-    # Count number of iterations using step size or
-    # step height h
-    n = (int)((x-x0)/h)
-    # Iterate for number of iterations
+def dydt2(t,y):
+    [S, I, R, Q] = y
+    dIdt = beta*I*S/N - gamma*I
+    dRdt = gamma*I          
+    dSdt = - beta*I*S/N - S*fear_function(I,dIdt,t)/N
+    dQdt =  S*fear_function(I, dIdt,t)/N    
+    dydt = [dSdt, dIdt, dRdt, dQdt]
+    
+    return np.array(dydt)
+
+
+def rk4(t,y0,t0,I_cutoff):
+    h=1e-1
+    n = (int)((t - t0)/h)
     y = y0
-    y_hist=[y0]
     for i in range(1, n+1):
-        "Apply Runge Kutta Formulas to find next value of y"
-        k1 =  [ h * model(y,x0,beta,N,sigma,gamma,mu)[i] for i in range (4)]
-        k2 =  [ h * model( y + 0.5 * k1, x0 + 0.5 * h,beta,N,sigma,gamma,mu)[i] for i in range (4)]
-        k3 =  [ h * model( y + 0.5 * k2,x0 + 0.5 * h,beta,N,sigma,gamma,mu)[i] for i in range (4)]
-        k4 =  [ h * model( y + k3,x0 + h,beta,N,sigma,gamma,mu)[i] for i in range (4)]
- 
-        # Update next value of y
-        y = y + (1.0 / 6.0)*(k1 + 2 * k2 + 2 * k3 + k4)
-        y_hist.append(y)
+        [S, I, R, Q] = y
         
-        # Update next value of x
-        x0 = x0 + h
-    return y_hist
+        if I<I_cutoff:
+            #print("#",end="")
+            S = S+Q
+            Q = 0
+            y = np.array([S, I, R, Q])
+            
+            k1 = h * dydt(t0, y)
+            k2 = h * dydt(t0 + 0.5 * h, y + 0.5 * k1)
+            k3 = h * dydt(t0 + 0.5 * h, y + 0.5 * k2)
+            k4 = h * dydt(t0 + h, y + k3)
+            y = y + (1.0 / 6.0)*(k1 + 2 * k2 + 2 * k3 + k4)
+            
+        else:
+            #print(".",end="")
+            k1 = h * dydt2(t0, y)
+            k2 = h * dydt2(t0 + 0.5 * h, y + 0.5 * k1)
+            k3 = h * dydt2(t0 + 0.5 * h, y + 0.5 * k2)
+            k4 = h * dydt2(t0 + h, y + k3)
+            y = y + (1.0 / 6.0)*(k1 + 2 * k2 + 2 * k3 + k4)
+        
+        t0 = t0 + h
+        
+    return y
 
-"""
-fears = np.linspace(0,1,100)
-
-def fear_fact(I):
-    I_cutoff = 1e5
-    if I>I_cutoff:
-        return 0
-    else:
-        return 
-    
-    if I>I_cutoff:
-        return ((I-I_cutoff)/(4*1e1*I_cutoff))**(2)+1
-    else:
-        return 1
-    
-    
-fears = []
-In = []
-
-for i in range(0,int(1e7),1000):
-    In.append(i)
-    fears.append(fear_fact(i))
-      
-plt.plot(In,fears)
-plt.show()
-"""
-
-beta= 0.34 #0.34 
-sigma= 0.1 
-gamma= 0.06
-mu= 0.005
+beta= 0.34  
+gamma= 0.1
 N = 1.3526e9
-birth_rate, death_rate = 28230/N ,28230/N # 28230/N , 77756/N
+I_cutoff = 1e4
 
-y_init = [N, 3, 3, 0]
+y_init = [N-2006, 2000, 6, 0]
 
-"""
-t = np.linspace(0,1000,500)
-sol = odeint(model, y_init, t, full_output=0,args=(beta,N,sigma,gamma,mu,
-                                     birth_rate,death_rate))
-"""
-t = np.linspace(0,500,501)
-sol=rk4(0,y_init,500,0.1,beta,N,sigma,gamma,mu)
+days = 300
+sol = np.zeros((days,4))
+t = np.zeros(days)
 
+for i in range (days):
+    sol[i,:] = rk4(i,y_init,0,I_cutoff)
+    t[i] = i
+
+print(sol)
+
+plt.figure()
 tot=sol[:,1]+sol[:,0]+sol[:,2]+sol[:,3]
-plt.plot(t, sol[:,1], 'b', label='Infectives')
+plt.plot(t, sol[:,1], 'b', label='Infectives') 
 #plt.plot(t,tot, label='total')
-plt.plot(t, sol[:,2], 'g', label='Recovered')
-plt.plot(t, sol[:,0], 'r', label='Susceptible')
-plt.plot(t, sol[:,3], 'black', label='Quarantined')
+#plt.plot(t, sol[:,2], 'g', label='Recovered')
+#plt.plot(t, sol[:,0], 'r', label='Susceptible')
+#plt.plot(t, sol[:,3], 'black', label='Quarantined')
 plt.legend(loc='best')
 plt.xlabel('time (days)')
 plt.grid()
 plt.show()
-
-
-infectives_obs = []
-total_recovered_obs = []
-time = []
-
-with open('case_time_series.csv','r') as csvfile:
-    t = 0
-    plots = csv.reader(csvfile, delimiter = ',')
-    for row in plots:
-        infectives_obs.append(int(row[2]))
-        total_recovered_obs.append(int(row[5]))
-        time.append(t)
-        t += 1
-"""        
-plt.plot(time[32:],infectives_obs[32:],label="Infectives observed")
-#plt.plot(time[32:],total_recovered_obs[32:], label = "Recovered observed")
-plt.legend(loc='best')
-plt.xlabel('time (days)')
-plt.grid()
-plt.show()
-"""
-
-
-
-
-
-
-
-
-
-
-
